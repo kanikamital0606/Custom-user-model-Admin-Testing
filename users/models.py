60c22d079a75fad86e6f3f451ccade8889335294
kanikamital0606 <kanikamittal0661996@gmail.com>
2021-08-31 04:26:42 +0530

    final commit

--- users/models.py
+++ users/models.py
@@ -1,10 +1,38 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import getting_lazy as _
-from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
-class NewUser(AbstractBaseUser):
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, email, user_name, first_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+        
+        return self.create_user(email, user_name, first_name, password, **other_fields)
+
+    def create_user(self, email, user_name, first_name, password, **other_fields):
+        if not email:
+            raise ValueError(_('You Must Provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, first_name=first_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class NewUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'),unique=True)
     user_name = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=100, blank=True)
@@ -12,3 +40,11 @@ class NewUser(AbstractBaseUser):
     about = models.TextField(_('about'),max_length=500,blank=True)
     is_staff =models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+
+    objects = CustomAccountManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name', 'first_name']
+    
+    def __str__(self):
+        return self.user_name
